016332447788187bc01757bf5d9e607992069450
ruslankarimov <yoursluzhba@gmail.com>
2020-04-19 03:04:33 +0300

    qualifier

---
+++ src/com/luv2code/springdemo/DatabaseFortuneService.java
@@ -0,0 +1,10 @@
+package com.luv2code.springdemo;
+
+public class DatabaseFortuneService implements FortuneService {
+
+    @Override
+    public String getFortune() {
+        return null;
+    }
+
+}
--- src/com/luv2code/springdemo/TennisCoach.java
+++ src/com/luv2code/springdemo/TennisCoach.java
@@ -6,21 +6,24 @@ import org.springframework.stereotype.Component;
 @Component
 public class TennisCoach implements Coach {
 
+    @Autowired
     private FortuneService fortuneService;
 
     public TennisCoach() {
         System.out.println(">> TennisCoach: inside default constructor");
     }
 
-    @Autowired
-    public void setFortuneService(FortuneService fortuneService) {
-        System.out.println(">> TennisCoach: inside default setFortuneService");
-        this.fortuneService = fortuneService;
-    }
-
-
 
     /*
+    @Autowired
+    public void doSomeCrazyStuff(FortuneService fortuneService) {
+        System.out.println(">> TennisCoach: inside default doSomeCrazyStuff");
+        this.fortuneService = fortuneService;
+    }
+    */
+
+    /*
+    @Autowired
     public TennisCoach(FortuneService fortuneService) {
         this.fortuneService = fortuneService;
     }
