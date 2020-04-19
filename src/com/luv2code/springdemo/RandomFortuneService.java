61ce14f05cb4a34e1abf0cc7efdf3adadcad029e
ruslankarimov <yoursluzhba@gmail.com>
2020-04-19 03:40:14 +0300

    qualifier2

--- src/com/luv2code/springdemo/RandomFortuneService.java
+++ src/com/luv2code/springdemo/RandomFortuneService.java
@@ -1,10 +1,22 @@
 package com.luv2code.springdemo;
 
+import java.util.Random;
+import org.springframework.stereotype.Component;
+
+@Component
 public class RandomFortuneService implements FortuneService {
+
+    private String[] data = {
+        "Beware of wolf in sheep's clothing",
+        "Dilligence is the mother of good luck",
+        "The journy is the reward"
+    };
+
+    private Random myRandom = new Random();
 
     @Override
     public String getFortune() {
-        return null;
+        int index = myRandom.nextInt(data.length);
+        return data[index];
     }
-
-}
+} 
